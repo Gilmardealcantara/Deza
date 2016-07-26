46a230a6080f895ecef92a05e799a7cda1ff782c
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-07-26 16:42:26 -0300

    Add bd conection

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask.ext.login import LoginManager
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-from app import views
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 lm = LoginManager()
 lm.login_view = "/login"
+app.secret_key = 's3cr3t'
 lm.init_app(app)
+
+@lm.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+
+from app import views
