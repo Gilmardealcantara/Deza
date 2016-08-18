a979f37d39a527b2445af1d4e1e037a1542e08c5
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-08-18 15:28:13 -0300

    Add method to csv downlod of the user list

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, jsonify, Response
 from app import app
 from forms import LoginForm
 from models import User
 from flask.ext.login import login_user, logout_user
 from app.utils.encode import sha512
 from app import lm
-
+import csv
 
 @lm.user_loader
 def load_user(id):
@@ -39,3 +39,27 @@ def logout():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route("/download")
+def hello():
+    return '''
+        <html><body>
+        Hello. <a href="/getPlotCSV">Click me.</a>
+        </body></html>
+        '''
+
+
+@app.route("/getPlotCSV")
+def getPlotCSV():
+    users = User.query.all()
+
+    csv = 'ID,NAME,EMAIL\n'
+    for user in users:
+        csv += str(user.id) + ',' + user.name + ',' + user.email + '\n' 
+
+    return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=myplot.csv"})
