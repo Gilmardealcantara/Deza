944761e58ef1f5f48feb9e25a7ad42091a3fe6f3
gilmardealcantara <gilmardealcantara@gmail.com>
2016-09-27 17:05:27 -0300

    Updade change form.

--- app/static/js/stacked.js
+++ app/static/js/stacked.js
@@ -34,12 +34,16 @@ function init() {
     setTimeout(function() {
         d3.json(stackedConfig.data_url, function(data) {
             spinner.stop();
-            data = cleanData(data)
-            stacked(data);
+            new_data = cleanData(data)
+            stacked(new_data);
+            style();
+
             $('form').change(function(){
             	$('svg')[0].remove()
             	updateGraph();
-            	init()            	
+            	new_data = cleanData(data)
+            	stacked(new_data);
+            	style();        	
             })
 
             $('.axis .tick text')
@@ -57,6 +61,7 @@ function init() {
 
 function updateGraph(){
 	var index;
+	//base : num max, domain 0 - base
 	if($('form input ')[0].checked){ // enrolled
 		graph.base = 40000000
 		graph.yIndex = 3;
@@ -65,16 +70,27 @@ function updateGraph(){
 		graph.base = 160000
 		graph.yIndex = 7;
 		graph.yName = 'Num Schools'
-	}else{
+	}else{ // classes
 		graph.base = 1600000
 		graph.yIndex = 2;
 		graph.yName = 'Classes'
 	}
 }
 
+function style(){
+   $('.axis .tick text')
+        .attr("font-size", "15px")
+        .attr("fill", "#666666")
+        .attr("font-weight", "normal")
+        .attr("font-family", "sans-serif");
+
+    $('.textlayers').attr("fill", "#1d3335");
+
+    $('.axis .tick line').attr("stroke", "#666666")
+}
+
 function cleanData(data){
 	//trada dados, year e values
-	// indice 6 bra_id, 3 matriculados
    	var aux = {};
    	data.data.forEach(function(row){
 		var location = row[6].slice(0,3);
@@ -109,6 +125,12 @@ function stacked(data){
 
 	var w = stackedConfig.width;
     var h = stackedConfig.height;
+   	var tip = d3.tip()
+	  .attr('class', 'd3-tip')
+	  .offset([-10, 0])
+	  .html(function(d) {
+	    return "<strong>Frequency:</strong> <span style='color:red'>100</span>";
+  })
 
 	var svg = d3.select('#' + stackedConfig.target)
 				.append("svg")
@@ -216,7 +238,6 @@ function stacked(data){
       		//.tickFormat(d3.formatPrefix(".1", 1e6))
       	);
 
-
     // now add titles to the axes
     // font-family="sans-serif" font-weight="normal" font-size="22px" fill="#444" dominant-graph.baseline="central" style="text-anchor: middle;
     svg.append("text")
@@ -236,6 +257,8 @@ function stacked(data){
         .attr("font-weight", "normal")
         .attr("font-family", "sans-serif")
         .text("Years");
+
+    layer.call(tip)
 }
 
 
